e5ff9e1c00448a7cb04221d2d6b420889ff9076a
Chneemann <andre-kempf@mailbox.org>
2024-12-17 21:53:34 +0100

    feat: background color added and player image integrated

--- game.py
+++ game.py
@@ -8,9 +8,18 @@ class Game:
         pygame.display.set_caption("ninja game")
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
+        
+        self.img = pygame.image.load("data/images/entities/player/idle/00.png")
+        self.img.set_colorkey((0, 0, 0))
+        
+        self.img_pos = [160, 260]
 
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+            
+            self.screen.blit(self.img, self.img_pos)
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
