52e35bee1b187f2191a5d5ff59a14353135c8342
Chneemann <andre-kempf@mailbox.org>
2024-12-18 09:10:13 +0100

    feat: update PhysicsEntity to handle collision detection with Tilemap; prevent objects from falling when standing on tiles

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,12 +8,32 @@ class PhysicsEntity:
         self.size = size
         self.velocity = [0, 0]
         
-    def update (self, movement=(0, 0)):
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+        
+    def update (self, tilemap, movement=(0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
         
         self.pos[0] += frame_movement[0]
-        self.pos[1] += frame_movement[1]
+        entiry_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entiry_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entiry_rect.right = rect.left
+                if frame_movement[0] < 0:
+                    entiry_rect.left = rect.right
+                self.pos[0] = entiry_rect.x
         
+        self.pos[1] += frame_movement[1]
+        entiry_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entiry_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entiry_rect.bottom = rect.top
+                if frame_movement[1] < 0:
+                    entiry_rect.top = rect.bottom
+                self.pos[1] = entiry_rect.y
+                
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
         
     def render(self, surf):
